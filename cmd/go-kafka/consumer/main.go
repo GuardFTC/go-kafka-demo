abcebcdcfa6b42ff794dd0ba2e9d4c029af33600
fengtiecheng <17615007230@163.com>
2025-09-04 14:13:24 +0800

    feat(consumer): 实现消费者优雅关闭
    - 添加信号监听，等待中断或终止信号
    - 使用 context.WithCancel 创建可取消的上下文
    - 在消费者中添加对上下文取消的监听
    - 使用 sync.WaitGroup 等待所有消费者 goroutine 结束
    - 优化消费者创建和消费逻辑，提高代码可读性和可维护性

--- cmd/go-kafka/consumer/main.go
+++ cmd/go-kafka/consumer/main.go
@@ -5,6 +5,10 @@ import (
 	"context"
 	go_kafka "go-kafka-demo/go-kafka"
 	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 // kafka服务地址
@@ -13,27 +17,78 @@ var addr = []string{"127.0.0.1:9092"}
 // Kafka默认Topic
 var defaultTopic = "test-go-topic"
 
-// 上下文
-var ctx = context.Background()
+// 类似于CountDownLatch 用于监听一组goroutine
+var wg sync.WaitGroup
+var goroutineNum = 0
 
 func main() {
 
-	//1.创建消费者组-1
-	createConsumer(1, "group1")
-	//go createConsumer(2, "group2")
+	//1.添加信号监听
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	//2.创建带取消功能的上下文
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	//3.创建消费者组-1
+	createConsumer(1, "group1", ctx)
+	createConsumer(2, "group1", ctx)
+	createConsumer(3, "group1", ctx)
+
+	//4.设置监听数量
+	wg.Add(goroutineNum)
+
+	//5.等待关闭信号
+	<-c
+	log.Println("receive close sign. try to closing consumers")
+
+	//6.收到关闭信号后，取消context，通知goroutine停止
+	cancel()
+
+	//7.等待goroutine结束
+	wg.Wait()
 }
 
 // createConsumer 创建消费者消费消息
-func createConsumer(id int, group string) {
+func createConsumer(id int, group string, ctx context.Context) {
 
-	//1.创建消费者
-	consumer := go_kafka.NewConsumer(id, ctx, addr, group, defaultTopic)
-	defer consumer.Close()
+	//1.消费者数量++
+	goroutineNum++
 
-	//2.监听消息
-	for {
-		if err := consumer.ConsumerMessage(); err != nil {
-			log.Printf("%s consume fail:%s", consumer.GetTitle(), err)
+	//2.创建协程，执行消费逻辑
+	go func() {
+
+		//3.确保最终可以释放锁资源
+		defer wg.Done()
+
+		//4.创建消费者
+		consumer := go_kafka.NewConsumer(id, ctx, addr, group, defaultTopic)
+		defer consumer.Close()
+
+		//5.在goroutine中处理消费
+		for {
+
+			//6.监听上下文是否被取消，如果被取消则优雅退出
+			select {
+			case <-ctx.Done():
+				log.Printf("%s closing", consumer.GetTitle())
+				return
+			default:
+			}
+
+			//7.未取消则继续消费
+			if err := consumer.ConsumerMessage(); err != nil {
+
+				//8.检测是否是因为上下文取消导致的异常，如果是，则优雅退出
+				if ctx.Err() != nil {
+					log.Printf("%s closing", consumer.GetTitle())
+					return
+				}
+
+				//9.如果不是主动退出导致的异常，则打印错误信息
+				log.Printf("%s consume fail:%s", consumer.GetTitle(), err)
+			}
 		}
-	}
+	}()
 }
